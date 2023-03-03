13bcd297445241764125fa0413586c6cd5fa8065
Steven Kim <stevenkkim@gmail.com>
2023-03-03 08:07:34 -0800

    Initial commit.

---
+++ starters/live-server-basic/src/PlayScene.js
@@ -0,0 +1,38 @@
+let player;
+let keys;
+
+class PlayScene extends Phaser.Scene {
+  constructor() {
+    super('PlayScene')
+  }
+
+  preload() {
+    this.load.image('background', 'assets/background.jpg');
+    this.load.image('cat', 'assets/cat.png');
+  }
+
+  create() {
+    this.add.image(0, 0, 'background').setOrigin(0, 0);
+    player = this.physics.add.sprite(800, 450, 'cat').setScale(.05);
+    keys = this.input.keyboard.addKeys('LEFT,RIGHT,UP,DOWN')
+  }
+
+  update() {
+    if (keys.LEFT.isDown) {
+      player.x -= 10;
+    }
+    if (keys.RIGHT.isDown) {
+      player.x += 10;
+    }
+    if (keys.UP.isDown) {
+      player.y -= 10;
+    }
+    if (keys.DOWN.isDown) {
+      player.y += 10;
+    }
+  }
+
+}
+
+
+export default PlayScene;
---
+++ starters/live-server-basic/src/game.js
@@ -0,0 +1,18 @@
+import PlayScene from "./PlayScene.js";
+
+export const config = {
+    type: Phaser.AUTO,
+    width: 1200,
+    height: 800,
+    backgroundColor: "#cccccc",
+    parent: "game",
+    scene: [PlayScene],
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 0 }
+        }
+    }
+}
+
+const game = new Phaser.Game(config)
