d2091e33f31bc6dd0064b20aca79e48777c0b8aa
Steven Kim <stevenkkim@gmail.com>
2023-03-05 15:43:30 -0800

    .

--- code/2-collisions/PlayScene.js
+++ code/2-collisions/PlayScene.js
@@ -6,6 +6,7 @@ export class PlayScene extends Phaser.Scene {
         this.keys;
         this.walls = [];
         this.wallsGroup;
+        this.kitties = [];
     }
 
     preload() {
@@ -27,7 +28,6 @@ export class PlayScene extends Phaser.Scene {
             .setBounce(1, 1)
 
 
-
         this.walls.push(
             this.physics.add.existing(
                 this.add.rectangle(0, 355, 320, 160, 0x000000, 0.2).setOrigin(0)
@@ -41,26 +41,32 @@ export class PlayScene extends Phaser.Scene {
         this.walls[0].body.setImmovable(true);
         this.walls[1].body.setImmovable(true);
 
+        for (let i = 0; i < 100; i++) {
+            this.kittiest[i] = this.physics.add.sprite(1200 * Math.random(), 800 * Math.random(), 'cat')
+                .setScale(.05)
+                .setCollideWorldBounds(true)
+                .setImmovable(false)
+                .setCircle(300)
+                .setOffset(120, 150)
+                .setBounce(2, 2)
+
+            this.kitties[i].body.setMaxSpeed(500)
+        }
 
 
-        this.player2 = this.physics.add.sprite(600, 300, 'cat')
-            .setScale(.05)
-            .setCollideWorldBounds(true)
-            .setImmovable(false)
-            .setCircle(300)
-            .setOffset(120, 150)
-            .setBounce(2, 2)
 
-        this.player2.body.setMaxSpeed(500)
-
-        this.physics.add.collider(this.player, this.player2, () => { console.log('hello') }, null, this)
         this.physics.add.collider(this.player, this.walls, () => { console.log('hello') }, null, this)
-        this.physics.add.collider(this.player2, this.walls, () => { console.log('hello') }, null, this)
-
+        this.physics.add.collider(this.kitties, this.player, () => { console.log('hello') }, null, this)
+        this.physics.add.collider(this.kitties, this.walls, () => { console.log('hello') }, null, this)
+        this.physics.add.collider(this.kitties, this.kitties, () => { console.log('hello') }, null, this)
     }
 
     update() {
-        if (this.keys.LEFT.isDown) {
+        if (this.keys.LEFT.isDown && this.keys.RIGHT.isDown) {
+            this.player.body.velocity.x = 0;
+            // this.player.x -= 10;
+        }
+        else if (this.keys.LEFT.isDown) {
             this.player.body.velocity.x = -300;
             // this.player.x -= 10;
         }
@@ -73,7 +79,11 @@ export class PlayScene extends Phaser.Scene {
         }
 
 
-        if (this.keys.UP.isDown) {
+        if (this.keys.UP.isDown && this.keys.DOWN.isDown) {
+            this.player.body.velocity.y = 0;
+            // this.player.y -= 10;
+        }
+        else if (this.keys.UP.isDown) {
             this.player.body.velocity.y = -300;
             // this.player.y -= 10;
         }
@@ -85,9 +95,8 @@ export class PlayScene extends Phaser.Scene {
             this.player.body.velocity.y = 0;
         }
 
-        this.player2.body.velocity.x *= 0.96;
-        this.player2.body.velocity.y *= 0.96;
-
+        // this.player.body.velocity.x *= 0.96;
+        // this.player.body.velocity.y *= 0.96;
 
     }
 
