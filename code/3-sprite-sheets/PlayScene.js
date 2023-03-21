7c27ec22b82802f563c1c569958853d2bb50f33b
Steven Kim <stevenkkim@gmail.com>
2023-03-20 20:33:10 -0700

    .

--- code/3-sprite-sheets/PlayScene.js
+++ code/3-sprite-sheets/PlayScene.js
@@ -11,10 +11,23 @@ export class PlayScene extends Phaser.Scene {
 
     preload() {
         this.load.setPath('../../assets/sprout-lands/');
-        this.load.spritesheet('grass', '/tilesets/grass.png', { frameWidth: 49, frameHeight: 49 });
+
+        /* https://newdocs.phaser.io/docs/3.54.0/focus/Phaser.Loader.LoaderPlugin-spritesheet
+        spritesheet(key, [url], [frameConfig], [xhrSettings])
+        frameConfig: object {
+            frameWidth: number		        The width of the frame in pixels.
+            frameHeight: number	<optional>	The height of the frame in pixels. Uses the frameWidth value if not provided.
+            startFrame: number	<optional>	The first frame to start parsing from.
+            endFrame: number	<optional>	The frame to stop parsing at. If not provided it will calculate the value based on the image and frame dimensions.
+            margin: number	    <optional>	The margin in the image. This is the space around the edge of the frames.
+            spacing: number	    <optional>	The spacing between each frame in the image.
+        }
+        */
+        this.load.image('grass', '/tilesets/grass.png');
+        this.load.spritesheet('grass-ss', '/tilesets/grass.png', { frameWidth: 48, frameHeight: 48 });
         // this.load.image('grass', '/tilesets/grass.png');
 
-        this.load.spritesheet('player', '/characters/player.png', { frameWidth: 49, frameHeight: 49 });
+        this.load.spritesheet('player', '/characters/player.png', { frameWidth: 48, frameHeight: 48 });
         // this.load.image('player', '/characters/player.png');
 
         this.load.setPath('../../assets/');
@@ -22,9 +35,15 @@ export class PlayScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.image(0, 0, 'grass').setOrigin(0, 0).setScale(2);
+
+        this.add.image(0, 0, 'grass').setOrigin(0, 0).setScale(2).setCrop(0, 0, 50, 50);
+        this.add.image(0, 0, 'grass-ss',).setOrigin(0, 0).setScale(2);
+
+
         this.add.tileSprite()
         this.keys = this.input.keyboard.addKeys('LEFT,RIGHT,UP,DOWN')
+
+
 
         this.player = this.physics.add.sprite(800, 450, 'player',)
             .setScale(2)
@@ -34,6 +53,62 @@ export class PlayScene extends Phaser.Scene {
             // .setOffset(120, 150)
             // .setFriction(.5)
             .setBounce(1, 1)
+            .setCrop()
+
+
+        const frameRate = 4;
+
+        this.anims.create({
+            key: 'down-idle',
+            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 1 }),
+            frameRate,
+            repeat: -1,
+        })
+        this.anims.create({
+            key: 'down-walk',
+            frames: this.anims.generateFrameNumbers('player', { start: 2, end: 3 }),
+            frameRate,
+            repeat: -1,
+        })
+        this.anims.create({
+            key: 'up-idle',
+            frames: this.anims.generateFrameNumbers('player', { start: 4, end: 5 }),
+            frameRate,
+            repeat: -1,
+        })
+        this.anims.create({
+            key: 'up-walk',
+            frames: this.anims.generateFrameNumbers('player', { start: 6, end: 7 }),
+            frameRate,
+            repeat: -1,
+        })
+        this.anims.create({
+            key: 'left-idle',
+            frames: this.anims.generateFrameNumbers('player', { start: 8, end: 9 }),
+            frameRate,
+            repeat: -1,
+        })
+        this.anims.create({
+            key: 'left-walk',
+            frames: this.anims.generateFrameNumbers('player', { start: 10, end: 11 }),
+            frameRate,
+            repeat: -1,
+        })
+        this.anims.create({
+            key: 'right-idle',
+            frames: this.anims.generateFrameNumbers('player', { start: 12, end: 13 }),
+            frameRate,
+            repeat: -1,
+        })
+        this.anims.create({
+            key: 'right-walk',
+            frames: this.anims.generateFrameNumbers('player', { start: 14, end: 15 }),
+            frameRate,
+            repeat: -1,
+        })
+
+
+        this.player.anims.play('down-idle')
 
 
         this.walls.push(
@@ -70,16 +145,18 @@ export class PlayScene extends Phaser.Scene {
     }
 
     update() {
+
+        const VELOCITY = 250;
         if (this.keys.LEFT.isDown && this.keys.RIGHT.isDown) {
             this.player.body.velocity.x = 0;
             // this.player.x -= 10;
         }
         else if (this.keys.LEFT.isDown) {
-            this.player.body.velocity.x = -300;
+            this.player.body.velocity.x = -VELOCITY;
             // this.player.x -= 10;
         }
         else if (this.keys.RIGHT.isDown) {
-            this.player.body.velocity.x = 300;
+            this.player.body.velocity.x = VELOCITY;
             // this.player.x += 10;
         }
         else {
@@ -92,16 +169,27 @@ export class PlayScene extends Phaser.Scene {
             // this.player.y -= 10;
         }
         else if (this.keys.UP.isDown) {
-            this.player.body.velocity.y = -300;
+            this.player.body.velocity.y = -VELOCITY;
             // this.player.y -= 10;
         }
         else if (this.keys.DOWN.isDown) {
-            this.player.body.velocity.y = 300;
+            this.player.body.velocity.y = VELOCITY;
             // this.player.y += 10;
         }
         else {
             this.player.body.velocity.y = 0;
         }
+
+
+        if (Phaser.Input.Keyboard.JustDown(this.keys.UP)) this.player.anims.play('up-walk')
+        else if (Phaser.Input.Keyboard.JustUp(this.keys.UP)) this.player.anims.play('up-idle')
+        else if (Phaser.Input.Keyboard.JustDown(this.keys.DOWN)) this.player.anims.play('down-walk')
+        else if (Phaser.Input.Keyboard.JustUp(this.keys.DOWN)) this.player.anims.play('down-idle')
+
+        if (Phaser.Input.Keyboard.JustDown(this.keys.LEFT)) this.player.anims.play('left-walk')
+        else if (Phaser.Input.Keyboard.JustUp(this.keys.LEFT)) this.player.anims.play('left-idle')
+        else if (Phaser.Input.Keyboard.JustDown(this.keys.RIGHT)) this.player.anims.play('right-walk')
+        else if (Phaser.Input.Keyboard.JustUp(this.keys.RIGHT)) this.player.anims.play('right-idle')
 
         // this.player.body.velocity.x *= 0.96;
         // this.player.body.velocity.y *= 0.96;
